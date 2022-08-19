f844431616865abf7026eb2f6484a1a7d5108df0
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-19 11:27:44 +0300

    commit tree 01724fbe67bdc462c96f537bd1778842bea96ab8

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+katz_deli=[]
+
+def line(katz_deli)
+    if katz_deli.empty?
+        puts "The line is currently empty." 
+    else 
+        line_status = "The line is currently:"
+        katz_deli.each.with_index(1) { |in_queue, pos| line_status << " #{pos}. #{in_queue}" }
+        puts line_status
+    end
+end
+
+#================================================================
+def take_a_number(katz_deli, last_customer)
+    katz_deli << last_customer
+    puts "Welcome, #{last_customer}. You are number #{katz_deli.size} in line." 
+end
+
+#================================================================
+def now_serving(katz_deli)
+    if katz_deli.empty?
+        puts "There is nobody waiting to be served!" 
+    else 
+        puts "Currently serving #{katz_deli.first}."
+        katz_deli.shift
+    end
+end
